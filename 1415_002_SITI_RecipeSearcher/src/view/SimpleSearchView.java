9397f6bcb1b78f228de5fc442e79692aaaf35633
gallastegui <gonzalo.gallastegui.penalba@gmail.com>
2015-02-24 18:29:46 +0100

    Actualization 24/02/2015: simpleSearchView

--- 1415_002_SITI_RecipeSearcher/src/view/SimpleSearchView.java
+++ 1415_002_SITI_RecipeSearcher/src/view/SimpleSearchView.java
@@ -13,10 +13,11 @@ public class SimpleSearchView extends JFrame
 	{
 		Color bk = new Color(210,225,240);
 		JPanel first = new JPanel(new GridLayout(5,1)), second = new JPanel(new BorderLayout()), third = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		JPanel wrapper = new JPanel(new FlowLayout(0, 0, FlowLayout.LEADING)), wrapper2 = new JPanel(new FlowLayout(FlowLayout.CENTER)), wrapper3 = new JPanel(new FlowLayout(0, 0, FlowLayout.LEADING)), wrapper4 = new JPanel(new FlowLayout(0, 0, FlowLayout.LEADING));
+		JPanel wrapper = new JPanel(new FlowLayout(0, 0, FlowLayout.LEADING)), wrapper2 = new JPanel(new FlowLayout(FlowLayout.CENTER)), wrapper3 = new JPanel(new FlowLayout(0, 0, FlowLayout.LEADING)), wrapper4 = new JPanel(new GridBagLayout()), wrapper5 = new JPanel(new GridBagLayout());
 		JLabel jlb1 = new JLabel("<html><h1><FONT COLOR=#4D2204>Buscador r&aacute;pido</FONT></h1></html>"), jlb2 = new JLabel("<html><h4><FONT COLOR=#4D2204>B&uacute;squeda por t&iacute;tulo:</FONT></h4></html>");
+		GridBagConstraints c = new GridBagConstraints();
 		JTextField jtfText1 = new JTextField(50);
-		JButton bttn = new JButton("Buscar");
+		JButton bttn = new JButton("Buscar"), bttn2 = new JButton("+"), bttn3 = new JButton("+");
 		String[] columnNames = {"First Name",
                 "Last Name",
                 "Sport",
@@ -40,10 +41,29 @@ public class SimpleSearchView extends JFrame
 		table2.setPreferredScrollableViewportSize(new Dimension(500, 70));
 		table2.setFillsViewportHeight(true);
 		JScrollPane scrollPane = new JScrollPane(table), scrollPane2 = new JScrollPane(table2);
-		wrapper3.add(scrollPane);
-		wrapper4.add(scrollPane2);
-		second.add(wrapper3, BorderLayout.WEST);
-		second.add(wrapper4, BorderLayout.EAST);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		wrapper5.add(scrollPane2,c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		wrapper5.add(bttn2, c);
+
+		c.fill = GridBagConstraints.VERTICAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		wrapper4.add(scrollPane,c);
+		c.fill = GridBagConstraints.VERTICAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		wrapper4.add(bttn3, c);
+		
+		//wrapper3.add(scrollPane);
+		wrapper4.add(scrollPane);
+		second.add(wrapper4, BorderLayout.WEST);
+		second.add(wrapper5, BorderLayout.EAST);
 		wrapper2.add(jlb1);
 		first.add(wrapper2);
 		first.add(jlb2);
@@ -60,6 +80,7 @@ public class SimpleSearchView extends JFrame
 		this.setSize(1000, 100);
 		setVisible(true);
 	}
+	
 	
 	public static void main(String args[]) {
 		new SimpleSearchView();
